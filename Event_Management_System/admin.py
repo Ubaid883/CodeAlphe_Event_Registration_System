40217ff27e35051af3da050b8ae5420f0e3948f0
ubaid <ubaidsaleem825@gmail.com>
2025-06-15 11:00:06 +0500

    add admin and models

--- Event_Management_System/admin.py
+++ Event_Management_System/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Events, Registration
 # Register your models here.
+# Event Admin 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title','description','date','location')
+admin.site.register(Events,  EventAdmin)
+
+#Register Admin
+class RegiserAdmin(admin.ModelAdmin):
+    list_display = ['user','event','register_at']
+
+admin.site.register(Registration, RegiserAdmin)
--- Event_Management_System/models.py
+++ Event_Management_System/models.py
@@ -9,3 +9,11 @@ class Events(models.Model):
     
     def __str__(self)->str:
         return self.title
+
+class Registration(models.Model):
+    user= models.CharField(max_length=50)
+    event = models.ForeignKey(Events, on_delete=models.CASCADE)
+    register_at = models.DateTimeField( auto_now_add=True)
+    
+    def __str__(self)->str:
+        return self.user
