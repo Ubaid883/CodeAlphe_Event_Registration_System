80188400683813d4a6860fe07e0688ce78e28712
ubaid <ubaidsaleem825@gmail.com>
2025-06-20 16:30:07 +0500

    create relationshio between models

--- Event_Management_System/admin.py
+++ Event_Management_System/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Events, RegistrationModel
 # Register your models here.
-# Event Admin 
-class EventAdmin(admin.ModelAdmin):
-    list_display = ('title','description','date','location')
-admin.site.register(Events,  EventAdmin)
 
 #Register Admin
 class RegisterAdmin(admin.ModelAdmin):
-    list_display = ['name','event', 'date']
+    list_display = ['Organization_name','event_name']
+    
 admin.site.register(RegistrationModel, RegisterAdmin)
+
+# Event Admin 
+class EventAdmin(admin.ModelAdmin):
+   list_display = ['title','description','date','location']
+admin.site.register(Events,  EventAdmin)
+
--- Event_Management_System/models.py
+++ Event_Management_System/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Events(models.Model):
-    title = models.CharField(max_length=20)
-    description = models.TextField()
-    date = models.DateTimeField()
-    location = models.CharField(max_length=255)
 
 #Registeration Model
 class RegistrationModel(models.Model):
-    name = models.CharField(max_length=50)
-    event = models.CharField(max_length=50)
-    date = models.DateTimeField(auto_now_add=False)
+    Organization_name = models.CharField(max_length=50)
+    event_name = models.CharField(max_length=50)
+
    
+# Event Model 
+class Events(models.Model):
+    title = models.ForeignKey(RegistrationModel, on_delete=models.CASCADE)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    
